a8e42d1086d982bfff6b58e0be72cc69e6ac597f
Thanapat Veerakajorn <t.veerakajorn@gmail.com>
2020-04-08 22:25:50 +0000

    updated environment.rb

--- sinatra_project/config/environment.rb
+++ sinatra_project/config/environment.rb
@@ -8,5 +8,8 @@ ActiveRecord::Base.establish_connection(
   :database => "db/#{ENV['SINATRA_ENV']}.sqlite"
 )
 
+Dir[File.join(File.dirname(__FILE__), "../app/models", "*.rb")].each {|f| require f}
+Dir[File.join(File.dirname(__FILE__), "../app/controllers", "*.rb")].each {|f| require f}
+
 require './app/controllers/application_controller'
 require_all 'app'
