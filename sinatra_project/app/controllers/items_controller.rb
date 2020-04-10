bb1297f795cde64b6ba874484fe5f1c7532463f5
Thanapat Veerakajorn <t.veerakajorn@gmail.com>
2020-04-10 17:55:56 +0000

    method to view items

--- sinatra_project/app/controllers/items_controller.rb
+++ sinatra_project/app/controllers/items_controller.rb
@@ -1,1 +1,11 @@
-class ExpensesController < ApplicationController
+class ItemsController < ApplicationController
+  
+  # lets a user view all items if logged in
+  # redirects to login page if not logged in
+  get '/items' do
+    if logged_in?
+      erb :'items/items'
+    else
+      redirect_if_not_logged_in
+    end
+  end
