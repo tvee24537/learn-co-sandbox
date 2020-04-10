6bd1e0ac684f5fe951a7d751619d88b24c03c3d0
Thanapat Veerakajorn <t.veerakajorn@gmail.com>
2020-04-10 17:16:59 +0000

    adding in get/post for '/lists'

--- sinatra_project/app/controllers/lists_controller.rb
+++ sinatra_project/app/controllers/lists_controller.rb
@@ -1,2 +1,26 @@
 class ListsController < ApplicationController
 
+  # lets user view expense lists if logged in
+  get '/lists' do
+    if logged_in?
+      @lists = current_user.lists.all
+      erb :'lists/lists'
+    else
+      redirect_if_not_logged_in
+    end
+  end
+
+  # does not let a user create a blank category
+  post '/lists' do
+    if params[:name].empty?
+      flash[:message] = "Please Enter a List Name"
+      redirect_to_lists
+    else
+      @user = current_user
+      @list = List.create(name:params[:name], user_id:@user.id)
+      redirect_to_lists
+    end
+  end
+
+  # displays a single list
+  
