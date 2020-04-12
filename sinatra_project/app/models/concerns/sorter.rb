614f30221ec2fc669d94c00c1781e6812300065b
Thanapat Veerakajorn <t.veerakajorn@gmail.com>
2020-04-12 23:55:56 +0000

    adding table sorted with date

--- sinatra_project/app/models/concerns/sorter.rb
+++ sinatra_project/app/models/concerns/sorter.rb
@@ -6,8 +6,8 @@ class Sorter
       self.items.collect {|item| item.amount}.sum
     end
 
-    def items_sort_by_amount
-      self.items.sort_by {|item| item[:amount]}.reverse
+    def items_sort_by_date
+      self.items.sort_by {|item| item[:date]}.reverse
     end
   end
 
