8e0e45381c76c768ffc57a066ea6be6fc0d7975a
Thanapat Veerakajorn <t.veerakajorn@gmail.com>
2020-09-13 15:14:05 +0000

    added validates presence of username and password in user model

--- sinatra_project/app/models/user.rb
+++ sinatra_project/app/models/user.rb
@@ -3,6 +3,7 @@ class User < ActiveRecord::Base
   include Quantifiable::InstanceMethods
   
   has_secure_password
+  validates_presence_of :username, :password
 
   has_many :lists, :dependent => :destroy
   has_many :items
