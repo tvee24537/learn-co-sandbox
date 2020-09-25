a01061d02d5764562336db176bd50a05ee92d16b
Thanapat Veerakajorn <t.veerakajorn@gmail.com>
2020-09-25 16:16:28 +0000

    adding comments to Quantifiable

--- sinatra_project/app/models/concerns/quantifiable.rb
+++ sinatra_project/app/models/concerns/quantifiable.rb
@@ -1,11 +1,11 @@
 class Quantifiable
 
   module InstanceMethods
-
+#Takes sum of items in list
     def total_amount
       self.items.collect {|item| item.amount}.sum
     end
-
+#Sort items in list by date
     def items_sort_by_date
       self.items.sort_by {|item| item[:date]}.reverse
     end
